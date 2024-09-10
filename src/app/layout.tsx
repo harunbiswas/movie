012dde59459ec1e84601d6f0c0f048c4e06222a8
Harun Biswas Rubel <harunbiswasrubel@gmail.com>
2024-09-11 04:21:05 +0600

    Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,6 @@
 import Header from "@/components/Header";
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "../assets/sass/style.scss";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Free movie download",
@@ -26,7 +14,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <head></head>
+      <body className={``}>
         <Header />
         {children}
       </body>
---
+++ src/components/Banner.tsx
@@ -0,0 +1,11 @@
+import Slider from "./slider/BannerSlider";
+
+export default function Banner() {
+  return (
+    <div className="banner">
+      <div className="contaner">
+        <Slider />
+      </div>
+    </div>
+  );
+}
