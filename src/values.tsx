0967fbecdb79ef408fa9c83bfe1b9475996c7184
Harun Biswas Rubel <harunbiswasrubel@gmail.com>
2024-11-21 14:29:32 +0600

    update

--- src/values.tsx
+++ src/values.tsx
@@ -4,7 +4,8 @@ type Values = {
 
 const values: Values = {
   // url: "http://localhost:4000",
-  url: "https://movie-backend-eosin.vercel.app",
+  // url: "https://movie-backend-eosin.vercel.app",
+  url: "https://backend.cinedroid.com",
 };
 
 export default values;
