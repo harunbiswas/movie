0b2fdeedb825a620ea551591348ebec39044ef3b
Harun Biswas Rubel <harunbiswasrubel@gmail.com>
2024-11-24 21:04:33 +0600

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,6 +41,23 @@ export default function RootLayout({
           `}
         </Script>
 
+        {/* Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '3185793954893543');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        {/* End Meta Pixel Code */}
+
         {/* Main App Layout */}
         <AppProvider>
           <Header />
